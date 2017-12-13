d7e1cf8b2e597b3de048409d53f52a710f0da891
atnartur <i@atnartur.ru>
2017-12-13 14:20:29 +0300

    + first

---
+++ ChairsGame/MainController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChairsGame
+{
+    public class MainController : Controller
+    {
+        // GET
+//        public IActionResult Index()
+//        {
+//             
+//        }
+    }
+}
